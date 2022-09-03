24c8c65d5e96f4a96ede0f231fe0b329299ec66f
SourceH <zizark4040@gmail.com>
2022-09-03 14:56:26 +0900

    food

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
 function App() {
 
   let Today = new Date(),
     Year = Today.getFullYear(),
-    Month = (Today.getMonth()+1),
+    Month = (Today.getMonth() + 1),
     Day = Today.getDate();
 
     Month = (Month < 10) ? '0' + Month : Month;
     Day = (Day < 10) ? '0' + Day : Day;
 
+  let API_TODAY = Year + Month + Day;
   let Today_show = Year + '.' + Month + '.' + Day;
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.post(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_NEIS_API_KEY}&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530254&MLSV_YMD=${API_TODAY}`)
+      .then((response) => {
+        setData(response.data);
+      });
+  }, [API_TODAY]);
+
+  if (!data) {
+    return console.log('로딩중...');
+  }
+
+  let MEAL = data.mealServiceDietInfo[1].row[0].DDISH_NM; 
+  let MEAL2 = MEAL.replace(/\s/g,'');
+  let MEAL3 = MEAL2.replace(/<br\/>/g, '\n'); 
+  let MEAL4 = MEAL3.replace(/ *\([^)]*\) */g, "");
   
   return (
     <div>
@@ -18,7 +40,7 @@ function App() {
       <div className="w-full px-3 grid gap-0 grid-cols-5 grid-rows-2">
         <div className="text-center text-[#787878]">
           <h1 className="font-gongb text-4xl">수학</h1>
-          <p>PM 12:00 ~ PM 12:50</p>
+          <p>6교시</p>
         </div>
 
         <div className="text-center text-[#787878]">
@@ -27,7 +49,7 @@ function App() {
 
         <div className="text-center text-[#f1f1f1]">
           <h1 className="font-gongb text-4xl">체육</h1>
-          <p className="text-[#787878]">AM 11:00 ~ AM 11:50</p>
+          <p className="text-[#787878]">5교시</p>
         </div>
 
         <div className="text-center text-[#787878]">
@@ -36,9 +58,8 @@ function App() {
 
         <div className="text-center text-[#787878]">
           <h1 className="font-gongb text-4xl">문학</h1>
-          <p>AM 10:00 ~ AM 10:50</p>
+          <p>4교시</p>
         </div>
-        
       </div>
 
       <div className="text-[#37393C] absolute right-52 rotate-45">
@@ -59,11 +80,7 @@ function App() {
 
       <h1 className="text-center font-gongb text-6xl text-[#787878] mt-20 mb-20">FOOD</h1>
       <div className="text-center text-[#f1f1f1] text-4xl">
-        <h1 className="mt-5 mb-5">김치볶음밥</h1>
-        <h1 className="mt-5 mb-5">달걀실파국</h1>
-        <h1 className="mt-5 mb-5">알감자조림</h1>
-        <h1 className="mt-5 mb-5">오이지무침</h1>
-        <h1 className="mt-5 mb-5">훈제오리부추무침</h1>
+        <h1 className="mt-5 mb-5 whitespace-pre-line leading-relaxed">{MEAL4}</h1>
       </div>
 
       <div className="text-center text-[#787878] text-sm mt-20">
