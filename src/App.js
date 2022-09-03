755667ec0f42344a885a80d544dd9936b943e304
SourceH <zizark4040@gmail.com>
2022-09-03 19:31:21 +0900

    Mobile Optimization

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    axios.post(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_NEIS_API_KEY}&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530254&MLSV_YMD=${API_TODAY}`)
+    axios.post(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_NEIS_API_KEY}&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530254&MLSV_YMD=20220902`) //현재 테스트로 2022년 09월 02일로 설정함 기존: ${API_TODAY}
       .then((response) => {
         setData(response.data);
       });
@@ -31,29 +31,30 @@ function App() {
   let MEAL2 = MEAL.replace(/\s/g,'');
   let MEAL3 = MEAL2.replace(/<br\/>/g, '\n'); 
   let MEAL4 = MEAL3.replace(/ *\([^)]*\) */g, "");
+
   
   return (
     <div>
 
-      <h1 className="text-center font-gongb text-6xl text-[#787878] mt-10 mb-20">TIMESTAMP</h1>
+      <h1 className="text-center font-gongb text-5xl sm:text-6xl text-[#787878] mt-10 mb-20">TIMESTAMP</h1>
 
-      <div className="w-full px-3 grid gap-0 grid-cols-5 grid-rows-2">
+      <div className="w-full px-3 grid gap-0 md:grid-cols-5 md:grid-rows-2 grid-cols-1 grid-rows-5">
         <div className="text-center text-[#787878]">
           <h1 className="font-gongb text-4xl">수학</h1>
           <p>6교시</p>
         </div>
 
         <div className="text-center text-[#787878]">
-          <h1 className="font-gongb text-4xl pt-2">{'<'}</h1>
+          <h1 className="font-gongb text-4xl md:pt-2 md:pl-0 pl-4 rotate-90 md:rotate-0">{'<'}</h1>
         </div>
 
         <div className="text-center text-[#f1f1f1]">
-          <h1 className="font-gongb text-4xl">체육</h1>
+          <h1 className="font-gongb text-4xl">전자회로</h1>
           <p className="text-[#787878]">5교시</p>
         </div>
 
         <div className="text-center text-[#787878]">
-          <h1 className="font-gongb text-4xl pt-2">{'<'}</h1>
+          <h1 className="font-gongb text-4xl md:pt-2 md:pl-0 pl-4 rotate-90 md:rotate-0">{'<'}</h1>
         </div>
 
         <div className="text-center text-[#787878]">
@@ -62,19 +63,19 @@ function App() {
         </div>
       </div>
 
-      <div className="text-[#37393C] absolute right-52 rotate-45">
+      <div className="text-[#37393C] absolute bottom-98 right-52 rotate-45 hidden lg:block">
         <i className="fa-solid fa-play fa-8x"></i>
       </div>
 
-      <div className="text-[#37393C] absolute bottom-32 right-52 rotate-45">
+      <div className="text-[#37393C] absolute bottom-32 right-52 rotate-45 hidden lg:block">
         <i className="fa-solid fa-square fa-8x"></i>
       </div>
 
-      <div className="text-[#37393C] absolute bottom-98 left-52 rotate-45">
+      <div className="text-[#37393C] absolute bottom-98 left-52 rotate-45 hidden lg:block">
         <i className="fa-solid fa-face-laugh fa-8x"></i>
       </div>
 
-      <div className="text-[#37393C] absolute bottom-32 left-52 rotate-45">
+      <div className="text-[#37393C] absolute bottom-32 left-52 rotate-45 hidden lg:block">
         <i className="fa-solid fa-book fa-8x"></i>
       </div>
 
